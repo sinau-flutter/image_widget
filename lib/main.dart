e9938d966078982547836107481ea04295178676
ayogatot <azerino25@gmail.com>
2019-10-23 14:12:34 +0700

    Adding Floating Button

--- lib/main.dart
+++ lib/main.dart
@@ -7,34 +7,47 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Image Widget'),
-          ),
-          body: Column(
-            children: <Widget>[
-              Flexible(
-                flex: 1,
-                child: Container(
-                  color: Colors.amber,
-                  child: Image(
-                    image: NetworkImage(
-                        "https://gigantic.store/wp-content/uploads/2019/01/flat-design-character-illustration.jpg"),
-                    fit: BoxFit.cover,
-                  ),
+        appBar: AppBar(
+          title: Text('Image Widget'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Container(
+                color: Colors.amber,
+                child: Image(
+                  image: NetworkImage(
+                      "https://gigantic.store/wp-content/uploads/2019/01/flat-design-character-illustration.jpg"),
+                  fit: BoxFit.cover,
                 ),
               ),
-              Flexible(
-                flex: 1,
-                child: Container(
-                  color: Colors.lightGreen,
-                  child: Image(
-                    image: AssetImage("images/flat-design-family.jpg"),
-                    fit: BoxFit.cover,
-                  ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                color: Colors.lightGreen,
+                child: Image(
+                  image: AssetImage("images/flat-design-family.jpg"),
+                  fit: BoxFit.cover,
                 ),
-              )
-            ],
-          )),
+              ),
+            )
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          child: Container(
+            height: 50.0,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => print('Button Pressed'),
+          tooltip: 'Yeah',
+          child: Icon(Icons.chat),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      ),
     );
   }
 }
