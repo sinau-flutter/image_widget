f1b4019e44edbccda0e43124db03d1e3ff59c410
ayogatot <azerino25@gmail.com>
2019-10-23 14:05:03 +0700

    Adding Asset and Network Image

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Image Widget'),
-        ),
-        body: Center(
-          child: Container(
-            child: Image(
-              image: NetworkImage(
-                  "https://gigantic.store/wp-content/uploads/2019/01/flat-design-character-illustration.jpg"),
-              fit: BoxFit.cover,
-            ),
+          appBar: AppBar(
+            title: Text('Image Widget'),
           ),
-        ),
-      ),
+          body: Column(
+            children: <Widget>[
+              Flexible(
+                flex: 1,
+                child: Container(
+                  color: Colors.amber,
+                  child: Image(
+                    image: NetworkImage(
+                        "https://gigantic.store/wp-content/uploads/2019/01/flat-design-character-illustration.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  color: Colors.lightGreen,
+                  child: Image(
+                    image: AssetImage("images/flat-design-family.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
